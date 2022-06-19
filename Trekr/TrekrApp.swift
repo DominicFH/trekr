83d521df773a90b6c8d0e64b88e613505e390832
Dominic <dominicfrancisharris@gmail.com>
2022-06-19 19:03:48 +0100

    Initial Commit

---
+++ Trekr/TrekrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekrApp.swift
+//  Trekr
+//
+//  Created by Dominic Harris on 19/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct TrekrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
